011c331b5fee99f3594aa6e9c6e7561b4f1c711a
yangsong8 <ysong8@ncsu.edu>
2018-08-10 15:27:40 -0400

    Created page_controller and 3 pages (home, contact, about)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about'
+  get 'contact_admin', to: 'pages#contact'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :tasks
   resources :courses
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 end
