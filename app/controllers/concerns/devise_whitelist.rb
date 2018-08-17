002a07f228dfe183b62f6b30e7e24fb05b006502
yangsong8 <ysong8@ncsu.edu>
2018-08-17 12:49:35 -0400

    Added select role drop down

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -5,7 +5,7 @@ module DeviseWhitelist
   end
   def configure_permitted_parameters
     # only need to add additional fields, default fields, such as email, do not have to be included
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :role])
   end
 end
