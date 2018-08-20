ef4ef388d31d4a11eedf75054c39339d68b456a0
yangsong8 <ysong8@ncsu.edu>
2018-08-20 11:01:26 -0400

    Allow admin to see all the courses created

--- app/controllers/instructor_tasks_controller.rb
+++ app/controllers/instructor_tasks_controller.rb
@@ -2,6 +2,10 @@ class InstructorTasksController < ApplicationController
   access instructor: [:list], instructor_pending: [:list], admin: :all
 
   def list
-    @courses = Course.where instructor_id: current_user.id
+    if logged_in?(:instructor,:instructor_pending)
+      @courses = Course.where instructor_id: current_user.id
+    elsif logged_in?(:admin)
+      @courses = Course.all
+    end
   end
 end
