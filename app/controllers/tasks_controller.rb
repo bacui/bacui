cc133da1d11e358891a43b672a623a6366ae7bbd
yangsong8 <ysong8@ncsu.edu>
2018-08-22 13:20:16 -0400

    Redirect to instructor_task/list after editing a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,11 +44,9 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
+        format.html { redirect_to({controller: "instructor_tasks", action: "list"}, notice: 'Task was successfully updated.') }
       else
         format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
