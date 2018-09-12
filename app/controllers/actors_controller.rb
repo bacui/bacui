31911bde915fb5a2da2d5184b165c1cdb59c5820
yangsong8 <ysong8@ncsu.edu>
2018-09-11 23:01:02 -0400

    Create a ActorsUser obj. once an actor is created successfully.

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,5 @@
 class ActorsController < ApplicationController
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
   access instructor: :all, instructor_pending: :all, admin: :all, student: :all
   def new
     @actor = Actor.new
@@ -10,10 +11,22 @@ class ActorsController < ApplicationController
     @actor = Actor.new(actor_params)
     respond_to do |format|
       if @actor.save
-        format.html {redirect_to({controller: "student_task", action: "list"}, notice: 'Team was successfully created.')}
+        ActorsUser.create(actor_id: @actor.id, user_id: current_user.id)
+        format.html {redirect_to({controller: "student_tasks", action: "list"}, notice: 'Team was successfully created.')}
       else
         format.html { render :new }
       end
     end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_actor
+    @actor = Actor.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def actor_params
+    params.require(:actor).permit(:title, :task_id)
   end
 end
