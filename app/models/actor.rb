2ef67b85264118d1108381adc14745e5ec77d245
yangsong8 <ysong8@ncsu.edu>
2018-09-13 23:16:14 -0400

    Added a full method to actor class.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,12 @@
 class Actor < ApplicationRecord
   belongs_to :task
   validates_presence_of :task_id
+  has_many :actors_users
+  has_many :users, through: :actors_users
+
+  def full?
+    task = Task.find(self.task_id)
+    users = self.users
+    users.size >= task.team_size
+  end
 end
