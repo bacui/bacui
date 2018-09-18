3c8433b9ef642e202a0e95ab4241d712269a4999
yangsong8 <ysong8@ncsu.edu>
2018-09-18 00:10:25 -0400

    Added code for creating artifacts.

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -6,12 +6,24 @@ class ArtifactsController < ApplicationController
   end
 
   def create
-
+    @artifact = Artifact.new(artifact_params)
+    respond_to do |format|
+      if @artifact.save
+        format.html {redirect_to({controller: "student_tasks", action: "list"}, notice: 'Artifact was successfully created.')}
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_artifact
     @artifact = Artifact.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def artifact_params
+    params.require(:artifact).permit(:content, :author_actor_id)
   end
 end
