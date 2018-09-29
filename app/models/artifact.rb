32625437f820e312bcdb3ddf8362731b6460b337
yangsong8 <ysong8@ncsu.edu>
2018-09-28 23:15:45 -0400

    Fixed the foreigh key for artifact model.

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,5 +1,4 @@
 class Artifact < ApplicationRecord
-  belongs_to :actor
-  validates_presence_of :task_id
+  validates_presence_of :author_actor_id
   belongs_to :author_actor, class_name: "Actor", foreign_key: "author_actor_id"
 end
