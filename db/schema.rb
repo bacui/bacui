6f6043e966400cc72838fdd66ee731aa2243d9bb
Pratik Abhyankar <abhyankarpratik@gmail.com>
2018-12-02 13:58:12 -0500

    Add type column in questions table, Modify edit_questionnaire view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_023211) do
+ActiveRecord::Schema.define(version: 2018_12_02_185307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_023211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "task_id"
+    t.string "type"
     t.index ["task_id"], name: "index_questions_on_task_id"
   end
 
